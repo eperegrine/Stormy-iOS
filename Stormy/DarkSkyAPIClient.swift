3d340464add58b10acdf5f10da7ba431ca6559f8
Emily Peregrine <peregrineemily@gmail.com>
2018-09-11 17:50:37 +0100

    completed network stack for get current weather

--- Stormy/DarkSkyAPIClient.swift
+++ Stormy/DarkSkyAPIClient.swift
@@ -20,7 +20,7 @@ class DarkSkyAPIClient {
     typealias WeatherCompletionHandler = (Weather?, DarkSkyError?) -> Void
     typealias CurrentWeatherCompletionHandler = (CurrentWeather?, DarkSkyError?) -> Void
     
-    private func getWeather(at coord: Coordinate, completionHandler completion: @escaping CurrentWeatherCompletionHandler ) {
+    private func getWeather(at coord: Coordinate, completionHandler completion: @escaping WeatherCompletionHandler ) {
         guard let url = URL(string: coord.description, relativeTo: baseUrl) else {
             completion(nil, DarkSkyError.invalidURL)
             return
@@ -29,8 +29,24 @@ class DarkSkyAPIClient {
         let request = URLRequest(url: url)
         
         let task = downloader.jsonTask(with: request) { json, error in
-//            guard let currently = json["currently"]
-            
+            guard let json = json else {
+                completion(nil, error)
+                return
+            }
+            guard let weather = Weather(json: json) else {
+                completion(nil, .jsonParsingError)
+                return
+            }
+            completion(weather, nil)
+        }
+        
+        task.resume()
+    }
+    
+    func getCurrentWeather(at coord: Coordinate, completionHandler completion: @escaping CurrentWeatherCompletionHandler) {
+        getWeather(at: coord) {
+            weather, error in
+            completion(weather?.currently, nil)
         }
     }
 }
